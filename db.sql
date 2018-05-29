865b13115ac7584861626dcdad5eb653864dfc87
fangcm <fangcm@gmail.com>
2018-05-29 16:19:09 +0800

    0

--- db.sql
+++ db.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS `sys_user`;
+
 CREATE TABLE `sys_user` (
   `id` varchar(32) NOT NULL COMMENT 'ID',
   `mobile` varchar(11) NOT NULL COMMENT '手机',
@@ -12,15 +14,15 @@ CREATE TABLE `sys_user` (
   `update_by` varchar(32) NOT NULL DEFAULT '' COMMENT '更新者',
   `del_flag` tinyint(2) NOT NULL DEFAULT '0' COMMENT '删除标志（0-正常，1-已删除）',
   PRIMARY KEY (`id`),
-  KEY `idx_mobile` (`mobile`,`del_flag`)
+  KEY `idx_mobile` (`mobile`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统-用户信息表';
 
+DROP TABLE IF EXISTS `sys_role`;
 
 CREATE TABLE `sys_role` (
   `id` varchar(32) NOT NULL COMMENT 'ID',
-  `name` varchar(255) NOT NULL COMMENT '角色名 以ROLE_开头',
-  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '角色显示标题',
-  `access` int(11) NOT NULL DEFAULT '0' COMMENT '对应权限值 权限菜单所需',
+  `name` varchar(255) NOT NULL COMMENT '角色名称',
+  `role_code` varchar(16) NOT NULL DEFAULT '' COMMENT '角色代码，例如 admin',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `create_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
@@ -29,6 +31,7 @@ CREATE TABLE `sys_role` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统-角色表';
 
+DROP TABLE IF EXISTS `sys_user_role`;
 
 CREATE TABLE `sys_user_role` (
   `id` varchar(32) NOT NULL COMMENT 'ID',
@@ -40,20 +43,18 @@ CREATE TABLE `sys_user_role` (
   `update_by` varchar(32) NOT NULL DEFAULT '' COMMENT '更新者',
   `del_flag` tinyint(2) NOT NULL DEFAULT '0' COMMENT '删除标志（0-正常，1-已删除）',
   PRIMARY KEY (`id`),
-  KEY `idx_userId_roleId` (`user_id`,`role_id`)
+  UNIQUE KEY `uni_roleId_userId` (`role_id`,`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统-用户角色表';
 
+DROP TABLE IF EXISTS `sys_menu`;
 
 CREATE TABLE `sys_menu` (
   `id` varchar(32) NOT NULL COMMENT 'ID',
   `name` varchar(64) NOT NULL DEFAULT '' COMMENT '菜单名称',
-  `title` varchar(64) NOT NULL DEFAULT '' COMMENT '标题',
   `icon` varchar(255) NOT NULL DEFAULT '' COMMENT '图标',
   `path` varchar(255) NOT NULL DEFAULT '' COMMENT '路径',
-  `component` varchar(255) NOT NULL DEFAULT '' COMMENT '前端组件',
   `root_level` bit(1) NOT NULL DEFAULT b'0' COMMENT '根级菜单',
   `parent_id` varchar(32) NOT NULL DEFAULT '' COMMENT '父菜单ID',
-  `access` int(11) NOT NULL DEFAULT '0' COMMENT '所需权限值',
   `sort` int(11) NOT NULL DEFAULT '0' COMMENT '排序（升序）',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `create_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建者',
@@ -62,4 +63,19 @@ CREATE TABLE `sys_menu` (
   `del_flag` tinyint(2) NOT NULL DEFAULT '0' COMMENT '删除标志（0-正常，1-已删除）',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统-菜单表';
+
+DROP TABLE IF EXISTS `sys_role_menu`;
+
+CREATE TABLE `sys_role_menu` (
+  `id` varchar(32) NOT NULL COMMENT 'ID',
+  `role_id` varchar(32) NOT NULL COMMENT '角色ID',
+  `menu_id` varchar(32) NOT NULL COMMENT '菜单ID',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `update_by` varchar(32) NOT NULL DEFAULT '' COMMENT '更新者',
+  `del_flag` tinyint(2) NOT NULL DEFAULT '0' COMMENT '删除标志（0-正常，1-已删除）',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uni_roleId_menuId` (`role_id`,`menu_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统-角色菜单表';
 
