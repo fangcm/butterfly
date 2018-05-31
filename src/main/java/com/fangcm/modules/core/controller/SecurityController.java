74efd66f611db1d34bcd14d44474d4f491de0966
fangcm <fangcm2008@qq.com>
2018-05-31 21:34:37 +0800

    1

--- src/main/java/com/fangcm/modules/core/controller/SecurityController.java
+++ src/main/java/com/fangcm/modules/core/controller/SecurityController.java
@@ -4,12 +4,10 @@ import com.fangcm.common.entity.Result;
 import com.fangcm.common.utils.JWTUtil;
 import com.fangcm.common.utils.ResultUtil;
 import com.fangcm.config.security.JWTToken;
+import com.fangcm.modules.core.entity.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Security相关接口
 @RestController
@@ -20,8 +18,8 @@ public class SecurityController {
      * 登录请求
      */
     @PostMapping("/login")
-    public Result<Object> submitLogin(String username, String password) {
-        JWTToken token = new JWTToken(JWTUtil.sign(username, password));
+    public Result<Object> submitLogin(@RequestBody(required=true) User u) {
+        JWTToken token = new JWTToken(JWTUtil.sign(u.getMobile(), u.getPassword()));
         SecurityUtils.getSubject().login(token);
         return new ResultUtil<Object>().setSuccessMsg("登录成功");
     }
